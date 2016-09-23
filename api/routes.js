03c8db42dc072a34f2cca4cd6e8a5b05070311f5
Tuong Tran <tuong.tran@outlook.com>
2016-09-23 11:57:29 +0700

    frontend - init dependencies

--- api/routes.js
+++ api/routes.js
@@ -3,10 +3,18 @@
  */
 
 'use strict';
+var path = require('path');
+var config = require('./config');
 
 module.exports = function(app) {
   // Insert routes below
   app.use('/api/auth', require('./auth'));
   app.use('/api/users', require('./controllers/user'));
   app.use('/api/records', require('./controllers/time-record'));
+
+  // All other routes should redirect to the index.html
+  app.route('/*')
+    .get((req, res) => {
+      res.sendFile(path.resolve(config.rootDir + '/front/index.html'));
+    });
 }
